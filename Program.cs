695ac9ff723b9add5eed31e49e81ca960a788fe3
Victor <govs79@outlook.com>
2024-03-09 19:10:28 +0300

    вычисление значений <=3

--- Program.cs
+++ Program.cs
@@ -7,11 +7,25 @@ for (int i =0; i<newArray.Length; i++) {
 newArray[i] = Console.ReadLine();
 }
 // Выборка элементов <=3
-
+int m = 0;
+for (int i =0; i<newArray.Length; i++) {
+    if (newArray[i].Length <= 3) {
+        m++;}
+    }
+string [] redlineArray = new string[m];
+for (int i=0;i<m;i++){
+    if (newArray[i].Length <= 3){
+        redlineArray[i] = newArray[i];
+    }
+   }
 
 // Вывод массива исходного
+Console.Write("Введенные элементы массива ");
+for (int i=0; i<newArray.Length; i++)
+{
+Console.Write(newArray[i]);}
+Console.Write("Элементы массива <= 3: ");
+for (int i=0; i<redlineArray.Length; i++)  {
+Console.Write(redlineArray[i]);}
 
-for (int i =0; i<newArray.Length; i++) {
-Console.WriteLine(newArray[i]);
-}
 return 0;
