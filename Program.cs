d2e741f9247394af7eae304a7e3743dc5b09e019
Victor <govs79@outlook.com>
2024-03-09 19:36:14 +0300

    улкчшен вывод результатов

--- Program.cs
+++ Program.cs
@@ -1,19 +1,24 @@
 ﻿//Ввод исходного массива
 Console.Write("Введите количество элементов массива: ");
 int n = Convert.ToInt32(Console.ReadLine());
+if (n<=0) {
+    Console.WriteLine("Ошибка ввода");
+    return 0;
+}
 string [] newArray = new string [n];
 Console.Write ($"Введите {n} элементов массива ");
 for (int i =0; i<newArray.Length; i++) {
 newArray[i] = Console.ReadLine();
 }
-// Выборка элементов <=3
+// Подсчет элементов <=3
 int m = 0;
 for (int i =0; i<newArray.Length; i++) {
     if (newArray[i].Length <= 3) {
         m++;}
     }
+// Занесение элементов <=3 в массив
 string [] redlineArray = new string[m];
-for (int i=0;i<m;i++){
+for (int i=0; i<m; i++) {
     if (newArray[i].Length <= 3){
         redlineArray[i] = newArray[i];
     }
@@ -23,9 +28,11 @@ for (int i=0;i<m;i++){
 Console.Write("Введенные элементы массива ");
 for (int i=0; i<newArray.Length; i++)
 {
-Console.Write(newArray[i]);}
-Console.Write("Элементы массива <= 3: ");
+Console.Write($"{newArray[i]}" + "||");}
+Console.WriteLine();
+// Вывод массива c элементами <=3
+Console.WriteLine("Элементы массива <= 3: ");
 for (int i=0; i<redlineArray.Length; i++)  {
-Console.Write(redlineArray[i]);}
+Console.WriteLine(redlineArray[i]);}
 
 return 0;
