9ee2b612fab0af12792078e34743a8505ece0a78
Victor <govs79@outlook.com>
2024-03-09 17:58:52 +0300

    вод и вывод массива

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿//Ввод исходного массива
+Console.Write("Введите количество элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string [] newArray = new string [n];
+Console.Write ($"Введите {n} элементов массива ");
+for (int i =0; i<newArray.Length; i++) {
+newArray[i] = Console.ReadLine();
+}
+// Выборка элементов <=3
+
+
+// Вывод массива исходного
+
+for (int i =0; i<newArray.Length; i++) {
+Console.WriteLine(newArray[i]);
+}
+return 0;
